c683b894ccb20cd3b677032bcf66c8627ce16d81
dmadon <deanna.madon@gmail.com>
2022-06-25 13:31:06 -0500

    add rows for time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,46 @@
+var listContainerEl = document.querySelector(".container");
+
 var currentDay = moment().format("dddd, MMMM D, YYYY");
 console.log(currentDay);
 
-var dayStart = moment().set('hour', 7).set('minute',00).format("h:mm a")
-console.log(dayStart)
-
-var setHour = moment(dayStart).format('h:mm a');
-console.log(setHour);
-
-var startOfHour = moment().startOf('hour').format('h:mm a');
-console.log(startOfHour);
-
-var currentHour = "10:00 pm";
-
-var test = function(){
-    if(startOfHour == currentHour){
-        console.log("true");
-    }
-    else{
-        console.log("false");
-    }
-}
-
-test();
-
-
 $("#currentDay").text(currentDay);
 
-   
+var timeBlocks = [
+    "7:00 am",
+    "8:00 am",
+    "9:00 am",
+    "10:00 am",
+    "11:00 am",
+    "12:00 pm",
+    "1:00 pm",
+    "2:00 pm",
+    "3:00 pm",
+    "4:00 pm",
+    "5:00 pm",
+    "6:00 pm"
+];   
 
-// var setTimeBlocks = function(dayStart){
-//     for(i=0; i<=10; i++){
-        
-        
-// }
-// }
+console.log(timeBlocks.length);
+
+
+var getTimeBlock = function(){
+
+    var listWrapperEl = document.createElement("ul");
+
+    for(i = 0; i < timeBlocks.length; i++){
+        console.log(timeBlocks[i]); 
+        var timeBlockEl = document.createElement("li");
+        timeBlockEl.classList=("row hour");
+        timeBlockEl.textContent=timeBlocks[i];
+        listWrapperEl.appendChild(timeBlockEl);
+    }
+    listContainerEl.appendChild(listWrapperEl);
+
+
+
+}
+
+getTimeBlock();
 
 
 
